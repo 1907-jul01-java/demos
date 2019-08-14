c036503b8504fb7d858ce866df9ff3884cf016fa
Mehrab <Mehrab.Rahman@Revature.com>
2019-08-14 11:35:14 -0500

    MSA-demo

---
+++ week-7-msa/Readme.md
@@ -0,0 +1,22 @@
+# Week 7 SOA/MSA Topics
+- SOAP
+    - WSDL
+        - definition, types, message, porttype, operation, binding, service, port
+        - Binding styles
+    - Contract first/contract last
+    - JAX-WS, JAXB
+    - Apache CXF
+    - SOAP envelope
+
+- SOA - Service Oriented Architecture
+
+- MSA - Microservice Architecture
+    - Advantages/disadvantages
+    - Microservices
+    - Infrastructure Services
+        - Gateway, Discovery, Registry
+        - Configuration, Circuit Breaker, Monitoring/Tracing
+        - Message Broker/Queue
+
+- Containerization (Docker)
+- Orchestration (Kubernetes)
---
+++ week-7-msa/msa-demo/hellomicro/src/main/java/com/revature/hellomicro/HellomicroApplication.java
@@ -0,0 +1,23 @@
+package com.revature.hellomicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class HellomicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellomicroApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "hello world!";
+	}
+
+}
